20e7aa821df773b90ebfebdd387c16ebd1438d8b
FelipeMDantas <felipemdantas@terra.com.br>
2021-11-30 23:56:24 -0300

    Implementation of the datocms-client dependency (community creation authenticated requests)

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -1,7 +1,20 @@
+import { SiteClient } from 'datocms-client';
+
 export default async function requestsReceiver(request, response){
-    const token = 'bb98848adb0a02c7707234c148702e'
-    console.log(token);
+    const token = 'bb98848adb0a02c7707234c148702e';
+    const client = new SiteClient(token);
+
+    const createdRecord = await client.items.create({
+        itemType: '1470509',
+        title: 'Comunidade de teste',
+        imageUrl: 'https://github.com/FelipeMDantas.png', 
+        creatorSlug: 'FelipeMDantas'
+    })
+
+    console.log(createdRecord);
+
     response.json({
-        data: 'Algum dado qualquer'
+        data: 'Algum dado qualquer',
+        createdRecord: createdRecord
     })
 }
